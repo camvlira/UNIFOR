cbe024e90f5329a9a431b331a1bc4d05a31116ac
camvlira <camillyvitorialira@gmail.com>
2026-03-05 16:25:40 -0300

    questao 1

---
+++ programiz.js
@@ -0,0 +1,19 @@
+let idade = prompt("Digite sua idade: ");
+
+if (idade >= 65){
+    console.log("O usuario pode se aposentar.");
+}
+else{
+    if(idade < 60){
+        console.log("O usuario não pode se aposentar.");
+    }
+    else{
+        let tempo = prompt("Digite seu tempo de contribuição: ");
+        if(idade >= 60 && tempo >= 30){
+        console.log("O usuario pode se aposentar.");
+        }
+        else{
+        console.log("O usuario não pode se aposentar.");
+        }
+    }
+}
